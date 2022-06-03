76ab72d4871bf8d207e18370c3be0d757f98b3d5
Michael Baron <baronmi@oregonstate.edu>
2022-06-03 17:12:04 -0400

    Simple bubble sort function in JavaScript

---
+++ bubbleSort.js
@@ -0,0 +1,22 @@
+// Algorthm
+
+const bubbleSort = function (numbers) {
+    const length = numbers.length;
+
+    for (let loop = 0; loop < length; loop++) {
+        for (let position = 0; position < length; position++) {
+            if (numbers[position] > numbers[position + 1]) {
+                // swap
+                const smallerNumber = numbers[position + 1];
+                numbers[position + 1] = numbers[position];
+                numbers[position] = smallerNumber;
+            }
+        }
+    }
+
+    return numbers;
+}
+
+// Testing
+let testArray = [240, 4, 69, 11, 62];
+console.log("Sorted? ", bubbleSort(testArray));
